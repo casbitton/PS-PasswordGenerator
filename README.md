c80dfa1aa9636d9b72c64bc87e3f897f7464eb46
Cas Bitton <casbitton@gmail.com>
2019-02-26 19:11:28 +1030

    Kick it!
    Quick 5min project to generate random passwords.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # PS-PasswordGenerator
+Powershell Password Generator
+
+Quick [xkcd](https://xkcd.com/936/) inspired script to generate temporary passwords 🔑.
+
+# Example
+
+`ps-pwgen.ps1`
+
+~~~
+
+How Many Passwords Would You Like?: 5
+
+5 fresh passwords comming right up!
+
+RoundMantisInfatuatedTermite
+GenerousOwlPalatableToast
+IgnorantEagleContentSarcastic
+StarlingStumbleDoozieRaccoon
+HappyNauticalDirectPremium
+
+~~~
+
+# BYO Word list 📔
+#### A few sample word lists to start things off
+
+Drop word lists into `words` folder.
+
+* https://github.com/xaviershay/mnemonicker-wordlist
+* https://github.com/sindresorhus/superb
+* https://github.com/boennemann/animals
+* https://github.com/datasets/world-cities
+* https://github.com/first20hours/google-10000-english
+
---
+++ ps-pwgen.ps1
@@ -0,0 +1,32 @@
+# Check for word lists
+if ((Get-ChildItem ./words/).count -eq 0) {
+    Write-Host "Hey, you forgot to add the word lists!" -ForegroundColor Yellow
+    exit
+}
+
+# Select number of passwords to generate
+$HowManyPasswords = Read-Host "How Many Passwords Would You Like?"
+Write-Host "$HowManyPasswords fresh passwords coming right up!
+" -ForegroundColor Green
+
+# Read word list
+$WordList = Get-Content ./words/*.txt
+
+# Shake the jar!
+$WordList = $WordList | Sort-Object {Get-Random}
+
+$ThisMany = 1..$HowManyPasswords
+
+foreach ($Password in $ThisMany) {
+    
+    # Get 4 random words from list
+    $GrabWords = Get-Random -InputObject $WordList -Count 4
+    
+    # Convert word to titlecase, make it human readable
+    $TextInfo = (Get-Culture).TextInfo
+    
+    # Output Password, Remove Spaces
+    $Output = $TextInfo.ToTitleCase($GrabWords) -replace '\s',''
+
+    $Output
+}
